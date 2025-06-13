5b965e7f73b326780455d0b6b8d66f37b938759e
Vishal Kumar <vs035967@gmail.com>
2025-06-14 03:12:20 +0530

    create generateToken method in AdminSchema.js file

--- Backend/model/AdminSchema.js
+++ Backend/model/AdminSchema.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 const AdminSchema = new mongoose.Schema({
     email: {
@@ -20,8 +23,28 @@ const AdminSchema = new mongoose.Schema({
     address: {
         type: String,
         required: true
-    }
+    },
+    tokens: [
+        {
+            token: {
+                type: String,
+                required: true
+            }
+        }
+    ]
 });
+
+
+AdminSchema.methods.generateToken = async function () {
+    const token = jwt.sign({
+        _id: this._id.toString(),
+    },
+        process.env.KEY
+    )
+    this.tokens = this.tokens.concat({ token });
+    await this.save();
+    return token;
+}
 
 const Admin = mongoose.model("Admin", AdminSchema);
 
